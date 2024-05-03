74c802598c6f6b08e6fae6666d920bc77ded0d57
Furb <jthabo@gmail.com>
2024-05-03 14:03:43 +0200

    removed headline from cases page

--- app/(front)/cases/page.tsx
+++ app/(front)/cases/page.tsx
@@ -4,11 +4,6 @@ import React from "react";
 const cases = async () => {
   return (
     <main className='page-wrap'>
-      <div className='custom-space col-span-full'>
-        <h1 className='text-primary'>
-          Events med grønne <br /> kvalitative løsninger
-        </h1>
-      </div>
       <div className='col-start-2 col-span-7 custom-space'>
         <Casegrid limit={12} orderBy={"_createdAt desc"} />
       </div>
