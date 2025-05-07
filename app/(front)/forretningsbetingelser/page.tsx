ecef1839a4dd577b82c083aa021174ac6c4f3724
Jesper <jthabo@gmail.com>
2025-05-07 09:17:14 +0200

    forretningsbetingelser

--- app/(front)/forretningsbetingelser/page.tsx
+++ app/(front)/forretningsbetingelser/page.tsx
@@ -1,5 +1,237 @@
 const Forretningsbetingelser = () => {
-  return <div>Forretningsbetingelser</div>;
+  return (
+    <main className="page-wrap">
+      <h2 className="custom-space-small text-center">
+        Generelle forretningsbetingelser
+      </h2>
+      <div className="bg-white md:bg-light rounded-xl py-24 md:py-48 wrap custom-space">
+        <ol className="list-decimal h4">
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">Kontrakten</h4>
+            <p>
+              Nærværende forretningsbetingelser er gældende i ethvert
+              aftaleforhold mellem Rethink Event v/Randel og arrangøren
+              medmindre andet er særskilt aftalt og fremgår af skriftlig aftale
+              mellem parterne. Ved arrangøren skal forstås den aftalepart, der
+              rekvirerer Rethink Event v/Randel til en opgave. Ved
+              ordrebekræftelse skal forstås den mellem Rethink Event v/Randel og
+              arrangøren indgåede aftale, som efter endeligt indgået aftale
+              bliver bekræftet skriftligt af Rethink Event v/Randel ved
+              fremsendelse af ordrebekræftelse. Ved optrædende skal forstås de
+              kunstnere, foredragsholdere eller andre eksterne bookede, som er
+              hyret af Rethink Event v/Randel til arrangementet. I forholdet
+              mellem Rethink Event v/Randel og arrangøren gælder, ud over
+              forretningsbetingelserne, alene det i ordrebekræftelsen anførte.
+              Enhver ændring eller tilføjelse til ordrebekræftelsen skal ske
+              skriftligt.
+            </p>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">Reklamation</h4>
+            <p>
+              Arrangøren er forpligtet til at kontrollere oplysningerne i
+              ordrebekræftelsen og straks gøre skriftlige indsigelser mod evt.
+              fejl heri. Enhver reklamation over det i ordrebekræftelsen
+              anførte, skal være Rethink Event v/Randel i hænde senest 8 dage
+              efter datoen anført i ordrebekræftelsen. Enhver senere reklamation
+              vil være at betragte som fremsat for sent.
+            </p>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-8 text-primary">Arrangørens forpligtelser</h4>
+
+            <ul className="list-disc list-outside md:list-inside max-w-2xl">
+              <li className="4 mb-8">
+                At indhente enhver nødvendig tilladelse til gennemførelse af det
+                aftalte arrangement, herunder fra offentlige myndigheder såvel
+                som fra private.
+              </li>
+              <li className="4 mb-8">
+                At sørge for, at der under hele arrangementet, samt under
+                klargøring, opsætning og nedtagning, forefindes en person med
+                den nødvendige tekniske kunnen til opstilling, gennemførelse og
+                nedtagning af arrangementet. Personen skal have fuld adgang til,
+                og være i stand til at betjene, stedets tekniske installationer,
+                herunder strømforsyning, teknikskabe, alarmer, mv.
+              </li>
+              <li className="4 mb-8">
+                At påse, at der ikke findes restriktioner mht. lydstyrke, idet
+                de optrædende dog tilpasser lydstyrken til lokalet.
+              </li>
+              <li className="4 mb-8">
+                At betale evt. ophavsretlige afgifter, herunder eventuel
+                Koda/Grammex eller lignende privatretlige rettighedshavere samt
+                evt. offentlige udgifter.
+              </li>
+              <li className="4 mb-8">
+                Ikke at foretage eller foranledige lyd- eller billedoptagelser
+                til andet end privat brug og i så tilfælde kun mod de
+                optrædendes accept.
+              </li>
+            </ul>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">
+              Anvendelse af Rethink Event v/Randels eller optrædendes udstyr mv.
+            </h4>
+            <p>
+              Lyd-/lysanlæg mv. opstillet af Rethink Event v/Randel eller de
+              optrædende må ikke anvendes af arrangøren uden forudgående
+              skriftlig aftale.
+            </p>
+          </li>
+
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">Afbestillingsbetingelser</h4>
+            <p className="mb-8">
+              Ved aftalens indgåelse er denne bindende i sin helhed. Aflyser
+              eller afbestiller arrangøren arrangementet er denne
+              erstatningsansvarlig overfor Rethink Event v/Randel efter dansk
+              rets almindelige regler, men skal som minimum betale følgende:
+            </p>
+            <ul className="list-disc list-outside md:list-inside max-w-2xl">
+              <li className="4 mb-8">
+                Ved afbestilling tidligere end 8 uger før arrangementet betales
+                50% af det samlede kontraktbeløb.
+              </li>
+              <li className="4 mb-8">
+                Ved afbestilling tidligere end 4 uger før arrangementet betales
+                75% af det samlede kontraktbeløb.
+              </li>
+              <li className="4 mb-8">
+                Ved afbestilling 4 uger før arrangementet eller senere betales
+                100% af det samlede kontraktbeløb. Såfremt en aftale vedrører
+                gennemførelse af mere end 1 arrangement, gælder bestemmelserne
+                om afbestilling i forhold til det arrangement der afholdes
+                først. Der skal dog betales kompensation i forhold til den
+                samlede aftales pris. Enhver ændring i forhold til det i
+                ordrebekræftelsen anførte betragtes som afbestilling og evt.
+                genbookning.
+              </li>
+              <li className="4 mb-8">
+                Såfremt arrangementet eller aftalen ikke kan gennemføres som
+                følge af COVID-19, anden pandemi, epidemi eller som følge af
+                konsekvenser afledt af nævnte forhold, er arrangøren/kunden
+                forpligtet til at dække omkostninger forbundet med
+                kontraktophævelsen, som leverandøren ikke kan afværge.
+                Leverandøren vedkender dog, at tabet ved kontraktophævelse
+                maksimalt udgør 100% af kontraktsummen, efter alle muligheder
+                for at begrænse tabet er afsøgt. Parterne skal så vidt muligt
+                dokumentere, at de hver især har iagttaget deres tabsbegrænsning
+                forpligtelse.
+              </li>
+            </ul>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">
+              Annulleringsret for Rethink Event v/Randel
+            </h4>
+            <p className="mb-8">
+              I de nedenstående situationer er Rethink Event v/Randel berettiget
+              til at annullere kontrakten helt eller delvist.
+            </p>
+            <ul className="list-disc list-outside md:list-inside max-w-2xl">
+              <li className="4 mb-8">
+                I tilfælde af sygdom eller dødsfald hos en af de optrædende
+                eller dennes familie.
+              </li>
+              <li className="4 mb-8">
+                Såfremt den/de optrædende hyres til et TV-arrangement på en
+                landsdækkende TV-station.
+              </li>
+              <li className="4 mb-8">
+                I tilfælde af, at Rethink Event v/Randel anser arrangementet for
+                uforsvarligt at gennemføre.
+              </li>
+              <li className="4 mb-8">
+                I tilfælde af offentlige forbud eller påbud, fogedforbud eller
+                lignende.
+              </li>
+            </ul>
+            <p>
+              Ved annullering af kontrakten er arrangøren indforstået med, at
+              Rethink Event v/Randel ikke kan gøres ansvarlig for arrangørens
+              eller dennes gæsters eller samarbejdspartneres tab af enhver art.
+              <br />
+              <br />
+              Arrangøren accepterer at friholde Rethink Event v/Randel for
+              ethvert krav fra 3. person i denne anledning. <br />
+              <br />
+              Rethink Event v/Randel er i tilfælde af annullering af kontrakten
+              berettiget til honorar for allerede udført arbejde Den/de
+              optrædende er endvidere berettiget til at undlade at optræde,
+              såfremt arrangøren ikke opfylder sine forpligtelser, jf. pkt. 3
+              ovenfor eller hvis de på anden måde forhindres i at udføre deres
+              optræden på forsvarlig vis.
+              <br />
+              <br /> I sådanne tilfælde er Rethink Event v/Randel berettiget til
+              det fulde kontraktbeløb.
+            </p>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">Ansvarsfraskrivelser</h4>
+            <p>
+              Rethink Event v/Randel kan ikke drages til ansvar for de
+              optrædendes handlinger, undladelser, kontraktbrud eller
+              forsinkelser. Arrangøren må derfor henholde sig til den/de
+              optrædende. Arrangøren er indforstået med, at Rethink Event
+              v/Randel ikke kan drages til ansvar for ethvert krav, enhver skade
+              eller enhver udgift, som måtte opstå direkte eller indirekte i
+              forbindelse med arrangementets gennemførelse. Rethink Event
+              v/Randel er alene ansvarlig for skade, fejl eller forsømmelser,
+              som beviseligt kan henføres til Rethink Event v/Randel. Dog kan
+              arrangørens krav på erstatning aldrig overstige aftalebeløbet for
+              arrangementet anført i ordrebekræftelsen. Rethink Event v/Randel
+              fraskriver sig ethvert ansvar for forhold, der indtræffer og som
+              ligger uden for Rethink Event v/Randels rådighed, herunder force
+              majeure, brand, ildsvåde, strejke, lock-out, handelshindringer,
+              politiske hindringer, råstofmangel, oversvømmelser, skybrud,
+              snestorm eller ethvert ikke blot usædvanligt, men tillige
+              ikke-forventeligt vejrforhold, naturkatastrofer, terror, uventede
+              prisstigninger på over 3%, samt ethvert andet force majeure
+              lignende forhold. Dette gælder også såfremt disse forhold
+              indtræffer for Rethink Event v/Randels leverandører, optrædende
+              eller andre samarbejdspartnere.
+            </p>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">Forsikringsforhold</h4>
+            <p>
+              Arrangøren er ansvarlig for at tegne relevante forsikringer til at
+              dække skader forvoldt af arrangøren eller dennes gæster på
+              udstyr/personale mv. fra Rethink Event v/Randel eller dennes
+              samarbejdspartnere, de optrædende eller andre, der har adgang til
+              arrangementet. Personale og optrædende udsendt af Rethink Event
+              v/Randel har ansvaret for at tegne relevante forsikringer til
+              dækning af eventuelle skader, der forvoldes af dem.
+            </p>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">Betalingsbetingelser</h4>
+            <p>
+              Ved ordrebekræftelse fremsendes faktura på betaling af depositum
+              udgørende minimum 20% af kontraktbeløbet. Beløbet refunderes ikke
+              i tilfælde af aflysning. Der faktureres derefter i henhold til
+              afbestillingsbetingelserne. Betaling forfalder som udgangspunkt
+              med 8 dages betalingsfrist. Der beregnes renter på 2% pr. måned
+              ved betaling efter forfaldstid.
+            </p>
+          </li>
+          <li className="custom-space-small">
+            <h4 className="mb-3 text-primary">Lovvalg og Værneting</h4>
+            <p className="mb-3">
+              Enhver tvist mellem Rethink Event v/Randel og arrangøren skal
+              afgøres efter dansk ret med Retten i Århus som værneting.
+            </p>
+            <p className="mb-3">
+              Rethink Event v/Randel. Fredens Torv 8.2, 8000 Aarhus C
+            </p>
+            <p>rethinkevent.dk | info@rethinkevent.dk | CVR nr. 3512 4616</p>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 };
 
 export default Forretningsbetingelser;
